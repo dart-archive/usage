f51cb3526ee1cba30dc9a963375261b7f189f29e
Devon Carew <devoncarew@gmail.com>
2014-12-21 11:35:31 -0800

    nits

--- lib/src/uuid.dart
+++ lib/src/uuid.dart
@@ -2,7 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// A UUID generator library.
+/**
+ * A UUID generator library.
+ */
 library usage.uuid;
 
 import 'dart:math' show Random;
