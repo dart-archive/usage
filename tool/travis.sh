9dcc074647457e80eff05a8d2b7cf8d93d2ab813
Devon Carew <devoncarew@gmail.com>
2014-12-31 10:11:29 -0800

    change the coveralls section

--- tool/travis.sh
+++ tool/travis.sh
@@ -36,14 +36,10 @@ dart tool/grind.dart build
 
 # Install dart_coveralls; gather and send coverage data.
 if [ "$REPO_TOKEN" ]; then
-  export PATH="$PATH":"~/.pub-cache/bin"
-
-  echo
-  echo "Installing dart_coveralls"
   pub global activate dart_coveralls
-
-  echo
-  echo "Running code coverage report"
-  # --debug for verbose logging
-  pub global run dart_coveralls report --token $REPO_TOKEN --retry 3 test/all.dart
+  pub global run dart_coveralls report \
+    --token $REPO_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/all.dart
 fi
